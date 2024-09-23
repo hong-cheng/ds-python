d7bcc5264b24f9c9abdc44881f848e9ff5059da2
Hong Cheng <hong.cheng@salesforce.com>
2024-09-23 09:36:03 -0400

    qq

--- pandas_numpy/pandas_cookbook.py
+++ pandas_numpy/pandas_cookbook.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
 import datetime as dt
+
+### create a simple array for dataframe
+
+data = np.random.randn(5,3)
+
+df = pd.DataFrame(data, index=['one','two','three','four','five'],
+                  columns=['A','B','C'])
+
+print(df)
